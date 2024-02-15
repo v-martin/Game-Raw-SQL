e03f004b288936251c85273027dce873feacddeb
goan_vi <gav6623@gmail.com>
2024-02-15 22:03:53 +0300

    add fix

--- game/game/settings.py
+++ game/game/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
     'rest_framework',
     'rest_framework.authtoken',
-
+    "corsheaders",
     'users',
     'play',
 ]
@@ -57,6 +57,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -83,6 +84,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'game.wsgi.application'
 
+CORS_ORIGIN_ALLOW_ALL = DEBUG
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
